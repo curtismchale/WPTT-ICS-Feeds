3b3c184f04fc40fd35875647155e27f6cec4f42b
curtismchale <curtis@curtismchale.ca>
2020-09-23 15:11:54 -0700

    adding basic test framework

---
+++ tests/test-post-quries.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Class TestPostQueries
+ *
+ * @package Wptt_Ics_Feeds
+ */
+
+/**
+ * Sample test case.
+ */
+class TestPostQueries extends WP_UnitTestCase {
+
+	/**
+	 * A single example test.
+	 */
+	public function test_sample() {
+		// Replace this with some actual testing code.
+		$this->assertTrue( true );
+	}
+}
